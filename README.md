da3530610cb48382c6c546501794ccc7686c0d3c
Mufeng <mufeng.xu@outlook.com>
2024-07-11 00:44:43 -0400

    Update README.md

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ because the center of the turning circle always lies on the extension of the rea
 
 ![Ackermann geometry](resources/modeling.png)
 
-### Kinematic Equations of The Vehicle
+<!-- ### Kinematic Equations of The Vehicle
 
 ```math
 \left\{\begin{align*}
@@ -25,8 +25,30 @@ because the center of the turning circle always lies on the extension of the rea
 \end{align*}\right.
 ```
 
-The changing rate of the turning angle $`\left|\omega\right| < 1 \ \mathrm{rad\!\cdot\! s^{-1}}`$.
+The changing rate of the turning angle $`\left|\omega\right| < 1 \ \mathrm{rad\!\cdot\! s^{-1}}`$. -->
 
 ### Simulation Environment
 
 The configuration file ([example](./src/utils/test_parking_lot.toml)) defines a test scene including obstacles, one parking slot and one vehicle.
+
+## Usage
+
+To get the shortest Reeds-Shepp curve from one point to another, run:
+```bash
+python reeds_shepp.py [-h] [-s X Y PHI] -e X Y PHI [-r RADIUS]
+```
+
+To run the simulation:
+```bash
+python ./src/simulations/simulator.py
+```
+You can change the scene setup and configurations in `src/utils/test_parking_lot.toml`
+or create a new configuration file.
+
+
+## Demos
+
+![](resources/parking-lot-1.png)
+![](resources/parking-lot-2.png)
+![](resources/parking-lot-3.png)
+![](resources/parking-lot-4.png)
--- src/simulations/simulator.py
+++ src/simulations/simulator.py
@@ -11,41 +11,38 @@ logger = logging.getLogger("[" + __name__ + "]")
 
 
 class Simulator:
-    def __init__(self, toml_file: str):
-        with open(toml_file, 'r') as f:
-            config = toml.loads(f.read())
-            logger.info('Read scene config: %s', toml_file)
+    def __init__(self, config: dict):
+        self.config = config
+        self.env = Environment(config["scene"])
+        self.car = Car(config["vehicle"])
 
-            self.env = Environment(config["scene"])
-            self.car = Car(config["vehicle"])
+        start_pose = config["planner"]["start_pose"]
+        goal_pose = config["planner"]["goal_pose"]
+        start_pose[2] *= np.pi / 180
+        goal_pose[2] *= np.pi / 180
 
-            start_pose = config["planner"]["start_pose"]
-            goal_pose = config["planner"]["goal_pose"]
-            start_pose[2] *= np.pi / 180
-            goal_pose[2] *= np.pi / 180
+        self.car.update(start_pose)
 
-            self.car.update(start_pose)
+        planner_params_ = {"env": self.env,
+                           "car": self.car,
+                           "start": start_pose,
+                           "goal": goal_pose,
+                           "angle_resolution": config["planner"]["angle_resolution"],
+                           "reverse_penalty": config["planner"]["reverse_penalty"],
+                           "error_goal_meter": config["planner"]["error_goal_meter"],
+                           "error_goal_radian": config["planner"]["error_goal_degree"] * np.pi / 180
+                           }
 
-            planner_params_ = {"env": self.env,
-                               "car": self.car,
-                               "start": start_pose,
-                               "goal": goal_pose,
-                               "angle_resolution": config["planner"]["angle_resolution"],
-                               "reverse_penalty": config["planner"]["reverse_penalty"],
-                               "error_goal_meter": config["planner"]["error_goal_meter"],
-                               "error_goal_radian": config["planner"]["error_goal_degree"] * np.pi / 180
-                               }
+        astar_params_ = {"heuristic_weight": config["astar"]["heuristic_weight"],
+                         "step_length": config["astar"]["step_length"]
+                         }
 
-            astar_params_ = {"heuristic_weight": config["astar"]["heuristic_weight"],
-                             "step_length": config["astar"]["step_length"]
-                             }
+        vehicle_params_ = {"minimum_turning_radius": self.car.minimum_turning_radius,
+                           "maximum_steering_angle": self.car.max_steering_angle,
+                           "wheelbase": self.car.wheelbase
+                           }
 
-            vehicle_params_ = {"minimum_turning_radius": self.car.minimum_turning_radius,
-                               "maximum_steering_angle": self.car.max_steering_angle,
-                               "wheelbase": self.car.wheelbase
-                               }
-
-            self.search = AstarSearch(planner_params_, astar_params_, vehicle_params_).search
+        self.search = AstarSearch(planner_params_, astar_params_, vehicle_params_).search
 
     def run(self):
         path = self.search()
@@ -58,12 +55,18 @@ class Simulator:
                 self.car.update(pose)
                 self.car.draw(fig, ax)
 
+            if self.config["result"]["save"]:
+                plt.savefig(self.config["result"]["path"])
+
             plt.show()
 
 
 if __name__ == '__main__':
     from pathlib import Path
-    file = Path(__file__).resolve()
-    config = str(file.parent.parent / 'utils/test_parking_lot.toml')
+    toml_file = str(Path(__file__).resolve().parent.parent / 'utils/test_parking_lot.toml')
 
-    Simulator(config).run()
+    with open(toml_file, 'r') as f:
+        configuration = toml.loads(f.read())
+        logger.info('Read scene config: %s', toml_file)
+
+        Simulator(configuration).run()
